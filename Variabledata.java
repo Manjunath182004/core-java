2a5ba4287fcc2cd06a5cb40315f4f8af70049c19
Manjunath182004 <its.appu18@gmail.com>
2024-05-26 00:33:17 +0530

    adding Variabledata file

---
+++ Variabledata.java
@@ -0,0 +1,77 @@
+public class Variabledata{
+    public static void main(String[] args){
+        //byte data types
+
+        byte sheNum = 100;
+        byte xeNum = 23;
+        byte xyzColur = 56;
+        byte fogNum = 90;
+        byte lovFor = 23;
+
+        System.out.println(sheNum);
+        System.out.println(xeNum);
+        System.out.println(xyzColur);
+        System.out.println(fogNum);
+        System.out.println(lovFor);
+
+        //short datatypes
+
+        short myAccBln = 25000;
+        short herNum = 5000;
+        short myLoss = 1500;
+
+        System.out.println(myAccBln);
+        System.out.println(herNum);
+        System.out.println(myLoss);
+
+        //int datatypes
+
+        int favNum = 18;
+        int birthDate = 2004;
+        int myAge = 21;
+
+        System.out.println(favNum);
+        System.out.println( birthDate);
+        System.out.println(myAge);
+
+    //long datatypes
+
+        long myNum = 9019195736L;
+        long myWts = 9019195736L;
+
+        System.out.println(myNum);
+        System.out.println(myWts);
+
+        //floating datatypes
+
+        float heHeight = 6.3f;
+        float myWeight = 56.37f;
+        float oilKg = 50.00f;
+
+        System.out.println(heHeight);
+        System.out.println(myWeight);
+        System.out.println(oilKg);
+
+        //double datatypes
+
+        double isNum = 90.88886d;
+        double curBill = 69.345364d;
+
+        System.out.println(isNum);
+        System.out.println(curBill);
+
+        //boolean datatypes
+
+        boolean isIamHandsomeBoy = true;
+        boolean yourBatteryIsDead = false;
+
+        System.out.println(isIamHandsomeBoy);
+        System.out.println(yourBatteryIsDead);
+
+        //char datatypes
+
+        char myGrade = 'A';
+        
+        System.out.println(myGrade);
+    }
+}
