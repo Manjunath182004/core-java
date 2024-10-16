41e9619a9ca65acce4d4ef411f4f9bbd3fda7ae9
Manjunath182004 <its.appu18@gmail.com>
2024-10-16 21:30:13 +0530

    This Detial Explanation about Variables in java With taking datatype int as a Example

---
+++ Variables.java
@@ -0,0 +1,161 @@
+// Variables are the containers used to store the values
+// syntax : datatype VariableName = value;
+// datatype ex are int, float, String, Double...etc
+// variables Ex on int ....
+
+public class Variables{
+	public static void main(String[] args){
+
+		int RollNo = 22;              
+		System.out.println(RollNo); 
+
+		RollNo = 23;               // resizing the variable
+		System.out.println(RollNo);
+
+		int Prize = 50;
+		System.out.println(Prize);
+		
+		Prize = 56;
+		System.out.println(Prize);
+
+		int Age = 21;
+		System.out.println(Age);
+
+		Age = 22;
+		System.out.println(Age);
+
+		int Height = 160;
+		System.out.println(Height);
+
+		Height = 165;
+		System.out.println(Height);
+
+		int Weight = 55;
+		System.out.println(Weight);
+
+		Weight = 58;
+		System.out.println(Weight);
+
+		int year = 2024;
+		System.out.println(year);
+
+		year = 2023;
+		System.out.println(year);
+
+		int Plotform = 2;
+		System.out.println(Plotform);
+
+		Plotform = 1;
+		System.out.println(Plotform);
+
+		int TokenNo = 22;
+		System.out.println(TokenNo);
+
+		TokenNo = 10;
+		System.out.println(TokenNo);
+
+		int Bill = 250;
+		System.out.println(Bill);
+
+		Bill = 300;
+		System.out.println(Bill);
+
+		int salary = 20000;
+		System.out.println(salary);
+
+		salary = 23000;
+		System.out.println(salary);
+
+		int GroupNo = 3;
+		System.out.println(GroupNo);
+
+		GroupNo = 2;
+		System.out.println(GroupNo);
+
+		int Atmpin = 34572;
+		System.out.println(Atmpin);
+
+		Atmpin = 22836;
+		System.out.println(Atmpin);
+
+		int HouseRent = 5000;
+		System.out.println(HouseRent);
+
+		HouseRent = 4000;
+		System.out.println(HouseRent);
+
+		int Bplevel = 120;
+		System.out.println(Bplevel);
+
+		Bplevel = 300;
+		System.out.println(Bplevel);
+
+		int AdharNo = 4567;
+		System.out.println(AdharNo);
+
+		AdharNo = 3456;
+		System.out.println(AdharNo);
+
+		int TotalDistric = 31;
+		System.out.println(TotalDistric);
+
+		TotalDistric = 34;
+		System.out.println(TotalDistric);
+
+		int alphabets = 26;
+		System.out.println(alphabets);
+
+		alphabets = 35;
+		System.out.println(alphabets);
+
+		int Bonus = 4000;
+		System.out.println(Bonus);
+
+		Bonus = 3000;
+		System.out.println(Bonus);
+
+		int SiteNo = 34;
+		System.out.println(SiteNo);
+
+		SiteNo = 29;
+		System.out.println(SiteNo);
+
+		int password = 20245;
+		System.out.println(password);
+
+		password = 23905;
+		System.out.println(password);
+
+		int Blance = 2000;
+		System.out.println(Blance);
+
+		Blance = 2030;
+		System.out.println(Blance);
+
+		int Amublance = 108;
+		System.out.println(Amublance);
+
+		Amublance = 107;
+		System.out.println(Amublance);
+
+		int police = 100;
+		System.out.println(police);
+
+		police = 102;
+		System.out.println(police);
+
+		int RoomNo = 343;
+		System.out.println(RoomNo);
+
+		RoomNo = 234;
+		System.out.println(RoomNo);
+
+		int error = 404;
+		System.out.println(error);
+
+		error = 102;
+		System.out.println(error);
+
+
+	}
+} 
