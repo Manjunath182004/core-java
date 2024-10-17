41a46d0d988fa5173404b0e25fa92ff705f01ef5
Manjunath182004 <its.appu18@gmail.com>
2024-10-17 12:15:32 +0530

    Datatypes(primitive datatypes)

---
+++ Datatypes.java
@@ -0,0 +1,286 @@
+public class Datatypes{
+	public static void main(String[] args) {
+		System.out.println("---------- datatype = byte ---------");
+
+		byte age = 21;
+        System.out.println(age);
+
+        byte noOfTeethInMouth = 32;
+        System.out.println(noOfTeethInMouth);
+
+        byte roomTemperature = 40;
+        System.out.println(roomTemperature);
+
+        byte percentage = 96;
+        System.out.println(percentage);
+
+        byte phValue = 15;
+        System.out.println(phValue);
+
+        byte monthsInYear = 12;
+        System.out.println(monthsInYear);
+
+        byte daysInMonth = 30;
+        System.out.println(daysInMonth);
+
+        byte statesInIndia = 29;
+        System.out.println(daysInMonth);
+ 
+        byte alphabets = 26;
+        System.out.println(alphabets);
+
+        byte nationalParkInIndia = 5;
+        System.out.println(nationalParkInIndia);
+
+        byte worldHeritageSiteInIndia =  43;
+        System.out.println(worldHeritageSiteInIndia);
+
+        byte ambulance = 108;
+        System.out.println(ambulance);
+
+        byte police = 100;
+        System.out.println(police);
+
+        byte healthCare = 104;
+        System.out.println(healthCare);
+
+        byte fireHelpline = 101;
+        System.out.println(fireHelpline);
+
+        byte hkDistrictInKarnataka = 7;
+        System.out.println(hkDistrictInKarnataka);
+
+        System.out.println("----------datatype = short ------------");
+
+        short countryInWorld = 195;
+        System.out.println(countryInWorld);
+
+        short year = 2024;
+        System.out.println(year);
+
+        short tigersInIndia = 3682;
+        System.out.println(tigersInIndia);
+
+        short studentsInCollege = 4066;
+        System.out.println(studentsInCollege);
+
+        short daysInYear = 365;
+        System.out.println(daysInYear);
+
+        short totalNoOfMinutesPerDay = 1440;
+        System.out.println(totalNoOfMinutesPerDay);
+
+        short booksInLibrary = 25000;
+        System.out.println(booksInLibrary);
+
+        short totalHardnessOfWater = 300;
+        System.out.println(totalHardnessOfWater);
+
+        short error = 404;
+        System.out.println(error);
+
+        short indiaIndipendentIn = 1947;
+        System.out.println(indiaIndipendentIn);
+ 
+        short totalRajyaSabhaSeats = 250;
+        System.out.println(totalRajyaSabhaSeats);
+
+        System.out.println("-----------datatype = int -----------");
+
+        int pincode = 583214;
+        System.out.println(pincode);
+
+        int password = 12345667;
+        System.out.println(pincode);
+
+        int salary = 60000;
+        System.out.println(salary);
+
+        int bikePrize = 452896673;
+        System.out.println(bikePrize);
+ 
+        int totalAreaOfKarnataka = 191791;
+        System.out.println(totalAreaOfKarnataka);
+
+        int totalSecondsPerDay = 86400;
+        System.out.println(totalSecondsPerDay);
+
+        int roomRent = 4079878;
+        System.out.println(roomRent);
+
+        int weightOfBlueWhale = 300000;
+        System.out.println(weightOfBlueWhale);
+
+        int iphonePrize = 100000;
+        System.out.println(iphonePrize);
+
+        int totalRiversInWorld = 150000;
+        System.out.println(totalRiversInWorld);
+
+        System.out.println("-----------datatype = long ---------");
+
+        long phoneNo = 9065783556l;
+        System.out.println(phoneNo);
+
+        long aadharNo = 678954347865l;
+        System.out.println(aadharNo);
+
+        long registerNo = 2024657867l;
+        System.out.println(registerNo);
+
+        long rationCardNo = 567326906546l;
+        System.out.println(rationCardNo);
+		
+		long oneLightYear = 9460730472580l;
+		System.out.println(oneLightYear);
+
+		long voterIdNo = 768172710180998l;
+		System.out.println(voterIdNo);
+
+		long totalAreaOfEarth = 51007200l;
+        System.out.println(totalAreaOfEarth);
+
+        long kohinoorDiamondPrize = 168060730400000l;
+        System.out.println(kohinoorDiamondPrize);
+
+        long ipAddress = 1123789382981090l;
+        System.out.println(ipAddress);
+
+        long bankAcNo = 565772877558809677l;
+        System.out.println(bankAcNo);
+
+        long speedOfLight = 299792458;
+        System.out.println(speedOfLight);
+
+        System.out.println("------------datatype = float --------------");
+
+        float height = 5.6f;
+        System.out.println(height);
+
+        float weight = 50.7f;
+        System.out.println(weight);
+
+        float acBalance = 200.45f;
+        System.out.println(acBalance);
+
+        float percen = 56.6f;
+        System.out.println(percen);
+
+        float cgpa = 8.9f;
+        System.out.println(cgpa);
+
+        float deathRateInIndia = 9.07f;
+        System.out.println(deathRateInIndia);
+
+        float birthRateInIndia = 16.27f;
+        System.out.println(birthRateInIndia);
+
+        float currentBill = 240.7f;
+        System.out.println(currentBill);
+
+        float intrest = 2.5f;
+        System.out.println(intrest);
+
+        float shortestPerson = 54.6f;
+        System.out.println(shortestPerson);
+
+        System.out.println("------------datatype = double --------------");
+
+        double piValue = 3.14159d;
+        System.out.println(piValue);
+
+        double gravityOfEarth = 9.80665d;
+        System.out.println(gravityOfEarth);
+
+        double bankBalance = 45000.90000d;
+        System.out.println(bankBalance);
+
+        double stockPrice = 150.567d;
+        System.out.println(stockPrice);
+
+        double loamAmt = 1500.879d;
+        System.out.println(loamAmt);
+
+        double averageScore = 45.678d;
+        System.out.println(averageScore);
+
+        double lightSpeed = 299792458.5674d;
+        System.out.println(lightSpeed);
+
+        double gdpRate = 7.4564d;
+        System.out.println(gdpRate);
+
+        double amtOfMercuryInWater = 0.001d;
+        System.out.println(amtOfMercuryInWater);
+
+        double wavelengthValue = 0.0019d;
+        System.out.println(wavelengthValue);
+
+        System.out.println("------------datatype = boolean --------------");
+
+        boolean isJavaEasy = false;
+        System.out.println(isJavaEasy);
+
+        boolean javaIsPlatformIndependent = true;
+        System.out.println(javaIsPlatformIndependent);
+  
+        boolean javaIsNotMultithreaded = false;
+        System.out.println(javaIsNotMultithreaded);
+
+        boolean carIsEasyToDrive = true;
+        System.out.println(carIsEasyToDrive);   
+
+        boolean isChickenTasty = true;
+        System.out.println(isChickenTasty);
+
+        boolean indiaIsDevloppingCountry = true;
+        System.out.println(indiaIsDevloppingCountry);
+
+        boolean dogHaveFiveLegs = false;
+        System.out.println(dogHaveFiveLegs);
+
+        boolean modhiIsPresidentOfIndia = false;
+        System.out.println(modhiIsPresidentOfIndia);
+
+        boolean javaHaveThreeDatatypes = false;
+        System.out.println(javaHaveThreeDatatypes);
+
+        boolean tigerIsNationalAnimalOfIndia = true;
+        System.out.println(tigerIsNationalAnimalOfIndia );
+
+
+        System.out.println("------------datatype = char --------------");
+
+
+        char currency = '$';
+        System.out.println(currency);
+
+        char grade = 'A';
+        System.out.println(grade);
+
+        char section = 'B';
+        System.out.println(section);
+
+        char block = 'c';
+        System.out.println(block);
+
+        char jobClass = 'D';
+        System.out.println(jobClass);
+
+        char gender = 'M';
+        System.out.println(gender);
+
+        char option = 'A';
+        System.out.println(option);
+
+        char firstLetterInAlphabet = 'A';
+        System.out.println(firstLetterInAlphabet);
+
+        char command = 'P';
+        System.out.println(command);
+
+        char trafficLight = 'R';   
+        System.out.println(trafficLight);
+
+	}
+}
