590518449fda76630a7674d42f49682f84bc7adf
Manjunath182004 <its.appu18@gmail.com>
2024-10-26 18:21:10 +0530

    Declaration and Invoke of Method

---
+++ Vijayanagara.java
@@ -0,0 +1,69 @@
+public class Vijayanagara{
+	 public static void main(String[] args) {
+
+	 	
+	 	System.out.println("                                       HISTORY OF VIJAYANAGARA                                ");
+	 	System.out.println("                                       ~~~~~~~~~~~~~~~~~~~~~~~                                ");
+
+	 	System.out.println("************************************************************************************************");
+
+	 	sangamaDynasty();
+
+	 	System.out.println("************************************************************************************************");
+
+	 	saluvaDynasty();
+
+	 	System.out.println("************************************************************************************************");
+
+	 	tuluvaDynasty();
+
+
+		
+	}
+
+	public static void sangamaDynasty(){
+
+		System.out.println("----------------------------Sangama Dynasty = 1336-1485----------------------------");
+
+		System.out.println("Founder : Harihara & Bukka in 1336");
+		System.out.println("Teacher : Vidyaranya who guide to build the kingdom");
+		System.out.println("Where Bukka his Brother Of Harihara");
+		System.out.println("Harihara is also known as Hakka");
+	    System.out.println(" Kings Comes Here are : Harihara II , Devaraya I, Devaraya II ,Etc. ");
+
+		
+
+	}
+
+	public static void saluvaDynasty(){
+		System.out.println("----------------------------Saluva Dynasty = 1485-1503----------------------------");
+
+		System.out.println("Saluva Narashimha : 1485-1490  ");
+		System.out.println("Started the ruling , he mainly focused restoring empires power");
+		System.out.println("Saluva Timma      : 1490-1503 ");
+		System.out.println("he continued and he faced more challenges.");
+
+
+
+	}
+
+	public static void tuluvaDynasty(){
+		System.out.println("----------------------------Tuluva Dynasty = 1485-1503----------------------------");
+		System.out.println("                                  (THE GOLDEN ERA)                                ");
+
+		System.out.println("Krishna Deva Raya : 1509-1529 ");
+		System.out.println("Most renowned ruler of the empire");
+		System.out.println("expanded the empire to its territorial extent,including Tamilnadu,karnataka,Apradesh.");
+		System.out.println("Achyuta Raya      : 1529-1542");
+		System.out.println(" Succeed his brother krishna deva raya");
+		System.out.println("He mainly focused on administrative reforms etc......");
+		System.out.println("Rama Raya         : 1542-1565 ");
+		System.out.println("Not a king in the traditional sense but the defactro ruler during the decline.");
+		System.out.println(" his failin the disastrous battel of Talikota in 1565 against Deccan Sultan");
+
+	}
+
+
+
+
+}
