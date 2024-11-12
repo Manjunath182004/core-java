5102dfc71aceea6d37d417c6b3478b7c1df64955
Manjunath182004 <its.appu18@gmail.com>
2024-11-12 18:46:34 +0530

    Arrays Simple Ex

---
+++ ArraysEx.java
@@ -0,0 +1,365 @@
+public class ArraysEx{
+	public  static void main(String[] args){
+
+
+		System.out.println("-----------------Datatype = byte---------------");
+
+		byte[] ages = {18,20,21,34,32,67,32,15,16,53}; // using literal Method
+
+		ages[3] = 56;  // Reassing Array
+		ages[6] = 38;
+		ages[5] = 37;
+
+		System.out.println(ages[0]);
+		System.out.println(ages[1]);
+		System.out.println(ages[2]);
+		System.out.println(ages[3]);
+		System.out.println(ages[4]);
+		System.out.println(ages[5]);
+		System.out.println(ages[6]);
+		System.out.println(ages[7]);
+		System.out.println(ages[8]);
+		System.out.println(ages[9]);
+
+		System.out.println("----------------------------------------------");
+
+		byte[] batteryLevel = new byte[10]; // using New Keyword Method
+
+		batteryLevel[0] = 20;
+		batteryLevel[1] = 23;
+		batteryLevel[2] = 56;
+		batteryLevel[3] = 100;
+		batteryLevel[4] = 58;
+		batteryLevel[5] = 34;
+		batteryLevel[6] = 37;
+		batteryLevel[7] = 90;
+		batteryLevel[8] = 14;
+		batteryLevel[9] = 15;
+
+		System.out.println(batteryLevel[0]);
+		System.out.println(batteryLevel[1]);
+		System.out.println(batteryLevel[2]);
+		System.out.println(batteryLevel[3]);
+		System.out.println(batteryLevel[4]);
+		System.out.println(batteryLevel[5]);
+		System.out.println(batteryLevel[6]);
+		System.out.println(batteryLevel[7]);
+		System.out.println(batteryLevel[8]);
+		System.out.println(batteryLevel[9]);
+
+
+		System.out.println("-----------------Datatype = short---------------");
+
+		short[] years = {2011,2012,2013,2014,2015,2016,2017,2018,2019,2020};
+
+		years[9] = 2024;
+		years[2] = 2001;
+		years[6] = 2004;
+
+		System.out.println(years[0]);
+		System.out.println(years[1]);
+		System.out.println(years[2]);
+		System.out.println(years[3]);
+		System.out.println(years[4]);
+		System.out.println(years[5]);
+		System.out.println(years[6]);
+		System.out.println(years[7]);
+		System.out.println(years[8]);
+		System.out.println(years[9]);
+
+		System.out.println("----------------------------------------------");
+
+		short[] stundentsInCollege = new short[10];
+
+		stundentsInCollege[0] = 2000;
+		stundentsInCollege[1] = 23223;
+		stundentsInCollege[2] = 5122;
+		stundentsInCollege[3] = 1000;
+		stundentsInCollege[4] = 5809;
+		stundentsInCollege[5] = 3438;
+		stundentsInCollege[6] = 3000;
+		stundentsInCollege[7] = 903;
+		stundentsInCollege[8] = 1431;
+		stundentsInCollege[9] = 1533;
+
+		System.out.println(stundentsInCollege[0]);
+		System.out.println(stundentsInCollege[1]);
+		System.out.println(stundentsInCollege[2]);
+		System.out.println(stundentsInCollege[3]);
+		System.out.println(stundentsInCollege[4]);
+		System.out.println(stundentsInCollege[5]);
+		System.out.println(stundentsInCollege[6]);
+		System.out.println(stundentsInCollege[7]);
+		System.out.println(stundentsInCollege[8]);
+		System.out.println(stundentsInCollege[9]);
+
+		System.out.println("-----------------Datatype = int---------------");
+
+		int[] salary = {520000,38000,100000,155000,200000,45000,60000,78000,52000,90000};
+
+		salary[7] = 57000;
+		salary[5] = 43000;
+		salary[3] = 89843;
+
+		System.out.println(salary[0]);
+		System.out.println(salary[1]);
+		System.out.println(salary[2]);
+		System.out.println(salary[3]);
+		System.out.println(salary[4]);
+		System.out.println(salary[5]);
+		System.out.println(salary[6]);
+		System.out.println(salary[7]);
+		System.out.println(salary[8]);
+		System.out.println(salary[9]);
+
+		System.out.println("----------------------------------------------");
+
+		int[] password = new int[10];
+
+        password[0] = 87646799;
+        password[1] = 83348823;
+		password[2] = 664653266;
+		password[3] = 83348823;
+		password[4] = 983704849;
+		password[4] = 490064596;
+		password[5] = 54565757;
+		password[6] = 89885495;
+		password[7] = 559655123;
+		password[8] = 68964848;
+		password[9] = 490064596;
+
+		System.out.println(password[0]);
+		System.out.println(password[1]);
+		System.out.println(password[2]);
+		System.out.println(password[3]);
+		System.out.println(password[4]);
+		System.out.println(password[5]);
+		System.out.println(password[6]);
+		System.out.println(password[7]);
+		System.out.println(password[8]);
+		System.out.println(password[9]);
+
+		System.out.println("-----------------Datatype = long---------------");
+
+		long[] aadharNo = {744567892345l,567823097845l,673456879012l,126745903456l,672389564903l,450974377889l,5879335974597l,5844680896489l,74638292082092l,86739280889393l};
+
+		aadharNo[1] = 416556646437l;
+		aadharNo[4] = 569893346466l;
+		aadharNo[8] = 797963256632l;
+
+
+		System.out.println(aadharNo[0]);
+		System.out.println(aadharNo[1]);
+		System.out.println(aadharNo[2]);
+		System.out.println(aadharNo[3]);
+		System.out.println(aadharNo[4]);
+		System.out.println(aadharNo[5]);
+		System.out.println(aadharNo[6]);
+		System.out.println(aadharNo[7]);
+		System.out.println(aadharNo[8]);
+		System.out.println(aadharNo[9]);
+
+		System.out.println("----------------------------------------------");
+
+		long[] registerNo = new long[10];
+
+        registerNo[0] = 695986566664l;
+		registerNo[1] = 641654563113l;
+		registerNo[2] = 454631354646l;
+		registerNo[3] = 896464464646l;
+		registerNo[4] = 454956665598l;
+		registerNo[5] = 211232435464l;
+		registerNo[6] = 695989566664l;
+		registerNo[7] = 434353545900l;
+		registerNo[8] = 772980820092l;
+		registerNo[9] = 776828929892l;
+		 
+
+		System.out.println(registerNo[0]);
+		System.out.println(registerNo[1]);
+		System.out.println(registerNo[2]);
+		System.out.println(registerNo[3]);
+		System.out.println(registerNo[4]);
+		System.out.println(registerNo[5]);
+		System.out.println(registerNo[6]);
+		System.out.println(registerNo[7]);
+		System.out.println(registerNo[8]);
+		System.out.println(registerNo[9]);
+
+		System.out.println("-----------------Datatype = float---------------");
+
+        float[] height = {5.6f, 4.8f, 3.4f,5.8f,4.5f,6.2f,3.6f,6.2f,8.4f,2.9f};
+
+        height[4] = 6.1f;
+        height[2] = 5.7f;
+        height[8] = 4.2f;
+
+        System.out.println(height[0]);
+		System.out.println(height[1]);
+		System.out.println(height[2]);
+		System.out.println(height[3]);
+		System.out.println(height[4]);
+		System.out.println(height[5]);
+		System.out.println(height[6]);
+		System.out.println(height[7]);
+		System.out.println(height[8]);
+		System.out.println(height[9]);
+
+
+		System.out.println("----------------------------------------------");
+
+		float[] cgpa = new float[10];
+
+		cgpa[0] = 8.9f;
+		cgpa[1] = 7.5f;
+		cgpa[2] = 8.3f;
+		cgpa[3] = 4.5f;
+		cgpa[4] = 6.7f;
+		cgpa[5] = 8.4f;
+		cgpa[6] = 6.5f;
+		cgpa[7] = 6.8f;
+		cgpa[8] = 9.4f;
+		cgpa[9] = 4.7f;
+
+		System.out.println(cgpa[0]);
+		System.out.println(cgpa[1]);
+		System.out.println(cgpa[2]);
+		System.out.println(cgpa[3]);
+		System.out.println(cgpa[4]);
+		System.out.println(cgpa[5]);
+		System.out.println(cgpa[6]);
+		System.out.println(cgpa[7]);
+		System.out.println(cgpa[8]);
+		System.out.println(cgpa[9]);
+
+		System.out.println("-----------------Datatype = double---------------");
+
+		double[] loanAmt = {1500.43232d, 5040.3233d, 5664.856d, 8632.4656d, 69.488562d,66866.25d,869.856d,566.5862d,9634.756d,55685.82356d};
+
+		loanAmt[2] = 43544.65657d;
+		loanAmt[3] = 33545.6557576d;
+		loanAmt[6] = 432.43546d;
+
+		System.out.println(loanAmt[0]);
+		System.out.println(loanAmt[1]);
+		System.out.println(loanAmt[2]);
+		System.out.println(loanAmt[3]);
+		System.out.println(loanAmt[4]);
+		System.out.println(loanAmt[5]);
+		System.out.println(loanAmt[6]);
+		System.out.println(loanAmt[7]);
+		System.out.println(loanAmt[8]);
+		System.out.println(loanAmt[9]);
+
+		System.out.println("----------------------------------------------");
+
+		double[] gdpRate = new double[10];
+
+		gdpRate[0] = 53.88956d;
+		gdpRate[1] = 83.5469d;
+		gdpRate[2] = 45353.34235d;
+		gdpRate[3] = 2343.544d;
+		gdpRate[4] = 56.65654d;
+		gdpRate[5] = 5676.5643d;
+		gdpRate[6] = 54.33212d;
+		gdpRate[7] = 568656.6516547d;
+		gdpRate[8] = 634.352355d;
+		gdpRate[9] = 5446.5432d;
+
+		System.out.println("-----------------Datatype = char---------------");
+
+		char[] alphabets = {'A' , 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+
+		System.out.println(alphabets[0]);
+		System.out.println(alphabets[1]);
+		System.out.println(alphabets[2]);
+		System.out.println(alphabets[3]);
+		System.out.println(alphabets[4]);
+		System.out.println(alphabets[5]);
+		System.out.println(alphabets[6]);
+		System.out.println(alphabets[7]);
+		System.out.println(alphabets[8]);
+		System.out.println(alphabets[9]);
+
+		System.out.println("----------------------------------------------");
+
+		char[] blocks = new char[10];
+
+		blocks[0] = 'A';
+		blocks[1] = 'C';
+		blocks[2] = 'G';
+		blocks[3] = 'Y';
+		blocks[4] = 'L';
+		blocks[5] = 'R';
+		blocks[6] = 'W';
+		blocks[7] = 'D';
+		blocks[8] = 'S';
+		blocks[9] = 'Z';
+
+		System.out.println(blocks[0]);
+		System.out.println(blocks[1]);
+		System.out.println(blocks[2]);
+		System.out.println(blocks[3]);
+		System.out.println(blocks[4]);
+		System.out.println(blocks[5]);
+		System.out.println(blocks[6]);
+		System.out.println(blocks[7]);
+		System.out.println(blocks[8]);
+		System.out.println(blocks[9]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+
+
+	}
+}
